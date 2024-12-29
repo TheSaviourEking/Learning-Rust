508be78f65401ac76e6d1c0f4f8e23cd656208b6
Saviour Eking <saviour.eking@outlook.com>
2024-12-29 08:22:53 +0000

    ref: clean up built targets
    The build targets are not needed in the repository

--- chapter-01-introduction/projects/hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb.d
+++
@@ -1,5 +0,0 @@
-/home/xo/workspace/rust/rust-pdf/hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb: src/main.rs
-
-/home/xo/workspace/rust/rust-pdf/hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb.d: src/main.rs
-
-src/main.rs:
--- chapter-01-introduction/projects/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/xo/workspace/rust/rust-pdf/hello_cargo/target/debug/hello_cargo: /home/xo/workspace/rust/rust-pdf/hello_cargo/src/main.rs
