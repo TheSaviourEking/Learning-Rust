a858cca0f8d3fad74f731912eef8d6908da5a179
TheSaviourEking <saviour.eking@outlook.com>
2024-12-29 18:23:12 +0100

    add initial variables project with Cargo configuration and example code

---
+++ chapter-03-common-programming-concepts/projects/variables/src/main.rs
@@ -0,0 +1,15 @@
+// Integer Data Types
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {x}");
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
+// Floating Point Data Types
+fn main() {
+    let x = 2.0; // f64
+    println!("The value of x is: {x}");
+    let y: f32 = 3.0; // f32
+    println!("The value of y is: {}", y);
+}
