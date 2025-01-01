4b53f47a9ceab9e236ca8c9e3be1171c714221f2
TheSaviourEking <saviour.eking@outlook.com>
2025-01-01 20:45:10 +0100

    add main function example with parameter passing in chapter 03

---
+++ chapter-03-common-programming-concepts/projects/functions/main.rs
@@ -0,0 +1,17 @@
+// fn main() {
+//     println!("Hello, World");
+
+//     another_function();
+// }
+
+// fn another_function() {
+//     println!("Another function.");
+// }
+
+fn main() {
+    another_function(5);
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
