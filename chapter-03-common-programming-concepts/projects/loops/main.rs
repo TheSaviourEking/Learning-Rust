8e598cd16502f3974c427556add57d22c614b4bb
TheSaviourEking <saviour.eking@outlook.com>
2025-01-07 11:13:24 +0100

    refactor loop example in chapter 03 to include a counter and return value

--- chapter-03-common-programming-concepts/projects/loops/main.rs
+++ chapter-03-common-programming-concepts/projects/loops/main.rs
@@ -1,6 +1,21 @@
+// fn main() {
+//     loop {
+//         println!("again!");
+//         println!("new loop")
+//     }
+// }
+
 fn main() {
-    loop {
-        println!("again!");
-        println!("new loop")
-    }
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+        println!("counter is {}", counter);
+
+        if counter == 20 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}")
 }
