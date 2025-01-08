6bf0c6548cae20b94ca98f0fa356c3d2eb6ec145
TheSaviourEking <saviour.eking@outlook.com>
2025-01-08 07:55:23 +0100

    add example of string manipulation in chapter 04

---
+++ chapter-04-understanding-ownership/exercises/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // let s = "hello"; // size is known at compile time -> stored on the stack
+    let mut s = String::from("hello"); //size is unknown at compile time -> stored on the heap
+
+    s.push_str(", world!");
+
+    println!("{s}");
+}
